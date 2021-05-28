0f5381a52c5d0d0766b56b0c5297a2eee22a61c7
“MatheusSArantes” <matheus.sodre.arantes@gmail.com>
2021-05-28 10:51:01 -0300

    Desafio3

---
+++ mod.js
@@ -0,0 +1,17 @@
+for(i = 1; i <=10; i++){
+    console.log(i)
+  }
+
+  i = 10
+while(i >= 1){
+  console.log(i)
+  i--
+}
+
+for(i = 1; i <=100; i+=2){
+    console.log(i)
+  }
+
+for(i = 0; i <=100; i+=2){
+    console.log(i)
+  }
